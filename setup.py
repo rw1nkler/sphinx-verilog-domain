f2dd5a164c98900974850a6f595fb1e4227a499f
Robert Winkler <rwinkler@antmicro.com>
2020-09-17 13:58:58 +0200

    Update the setup.py for tests
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sphinx-verilog-domain",
-    version="0.0.2",
-    author="SymbiFlow Authors",
-    author_email="symbiflow@lists.librecores.org",
-    description="Verilog Domain for Sphinx",
+    name="zaqwsxcde-test-package",
+    version="0.0.1",
+    author="Robert Winkler",
+    description="Verilog Domain for Sphinx test Package",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/SymbiFlow/sphinx-verilog-domain",
+    url="https://github.com/rw1nkler/sphinx-verilog-domain",
     python_requires=">=3.7",
     packages=setuptools.find_packages(),
     package_data={'': ["verilog.lark"]},
